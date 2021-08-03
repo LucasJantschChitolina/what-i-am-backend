99ebc5b66fefe082b0d1ee324390400cef654dec
Lucas Chitolina <lucas.chitolina01@gmail.com>
2021-08-03 20:02:40 -0300

    adding Procfile to support Heroku deploy :rocket:

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { getDatabase } from './index.js'
 import cors from 'cors';
 import dotenv from 'dotenv'
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 dotenv.config()
